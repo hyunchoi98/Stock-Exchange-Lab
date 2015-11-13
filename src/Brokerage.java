f1bafdd2937366e34fb3aa6852ae0a8c242b5ce9
hyunchoi98 <hyunchoi98@gmail.com>
2015-11-13 13:16:38 -0500

    added brokerage

--- src/Brokerage.java
+++ src/Brokerage.java
@@ -12,8 +12,24 @@ public class Brokerage implements login {
 	//adds this trader to the map of traders(screen name as key)
 	public int addUser(String name, String password){};
 	
+	//Tries to login a trader with a given screen name and password.
 	public int login(String name, String password){}
 	
+	//Removes a specified trader from the set of logged-in traders.
+	public void logout (Trader trader) {
+		
+	}
+	
+	//Requests a quote for a given stock from the stock exachange and passes it
+	//along to the trader by calling trader's receiveMessage method.
+	public void getQuote(String symbol, Trader trader) {
+		
+	}
+	
+	//Places an order at the stock exchange.
+	public void placeOrder(TradeOrder order) {
+		
+	}
 	
 	
 	
