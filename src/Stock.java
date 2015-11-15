22ebbf951ee8f2395beb1815308b4e22438272cd
JivanP <JivanPurutyan@Jivans-MacBook-Air-2.local>
2015-11-15 18:34:25 -0500

    Added Stock

--- src/Stock.java
+++ src/Stock.java
@@ -3,12 +3,29 @@
 
 public class Stock {
 	
+	private String stockSymbol; 
+	private String companyName;
+	private double lowestSell;
+	private double highestSell;
+	private int volumeDay;
+	private double stockPrice;
+	
+	
 //Constructs a new stock with a given symbol, 
 //company name, and starting price.
 //creates a priority queue to sell and buy orders both with  
 //PriceComparator method
 public Stock(String symbol, String name, double price)
-{}
+{
+	stockSymbol = symbol;
+	companyName = name;
+	lowestSell = price;
+	highestSell = price;
+	stockPrice = price;
+	volumeDay = 0;
+	Queue <TradeOrder> buy = new PriorityQueue<TradeOrder>();
+	Queue <TradeOrder> sell = new PriorityQueue<TradeOrder>();
+}
 
 //Returns a quote string for this stock. 
 //includes company name, stock symbol, last sale price
@@ -18,11 +35,12 @@ public Stock(String symbol, String name, double price)
 public String getQuote()
 {
 	//sample output
+	//Giggle.com (GGGL) Price: 12.00 hi: 14.50 lo: 9.00 vol: 
+	//500 Ask: none Bid: 12.50 size: 200
 	
-	String x ="Giggle.com (GGGL) Price: 12.00 hi: 14.50 lo: 9.00 "
-			+ "vol: 500 Ask: none Bid: 12.50 size: 200";
 	
-	return x; 	
+	return "symbol: " + stockSymbol + " Price: " + stockPrice +
+			"hi" + highestSell + "low" + lowestSell + "vol" + volumeDay; 	
 }
 
 //Places a trading order, adds order to apporpirate priority 
