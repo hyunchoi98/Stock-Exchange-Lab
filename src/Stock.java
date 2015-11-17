729a21ce26e5268eb30166f5748dc5e0e1c7b1bb
JivanP <JivanPurutyan@Jivans-MacBook-Air-2.local>
2015-11-16 22:02:09 -0500

    Updated Stock

--- src/Stock.java
+++ src/Stock.java
@@ -12,8 +12,11 @@ public class Stock {
 	private double highestSell;
 	private int volumeDay;
 	private double stockPrice;
+	Queue <TradeOrder> buy = new PriorityQueue<TradeOrder>();
+	Queue <TradeOrder> sell = new PriorityQueue<TradeOrder>();
 	
 	
+<<<<<<< Updated upstream
 	//Constructs a new stock with a given symbol, 
 	//company name, and starting price.
 	//creates a priority queue to sell and buy orders both with  
@@ -26,9 +29,35 @@ public class Stock {
 		highestSell = price;
 		stockPrice = price;
 		volumeDay = 0;
-		Queue <TradeOrder> buy = new PriorityQueue<TradeOrder>();
-		Queue <TradeOrder> sell = new PriorityQueue<TradeOrder>();
 	}
+
+//Constructs a new stock with a given symbol, 
+//company name, and starting price.
+//creates a priority queue to sell and buy orders both with  
+//PriceComparator method
+public Stock(String symbol, String name, double price)
+{
+	stockSymbol = symbol;
+	companyName = name;
+	lowestSell = price;
+	highestSell = price;
+	stockPrice = price;
+	volumeDay = 0;
+	Queue <TradeOrder> buy = new PriorityQueue<TradeOrder>();
+	Queue <TradeOrder> sell = new PriorityQueue<TradeOrder>();
+}
+
+//Returns a quote string for this stock. 
+//includes company name, stock symbol, last sale price
+//lowest and highest day prices, lowest price in a sell order,
+//the number of shares in it, highest price in buy order and num shares
+//in it
+public String getQuote()
+{
+	//sample output
+	//Giggle.com (GGGL) Price: 12.00 hi: 14.50 lo: 9.00 vol: 
+	//500 Ask: none Bid: 12.50 size: 100
+>>>>>>> Stashed changes
 	
 	//Returns a quote string for this stock. 
 	//includes company name, stock symbol, last sale price
@@ -49,7 +78,14 @@ public class Stock {
 	//queue depending on if it is a buy or sell. Notifies the trader 
 	//that placed the order that it has been placed by sending a message
 	public void placeOrder(TradeOrder order) {
+		if (order.isSell())
+			sell.add(order);
+		if (!order.isSell())
+			buy.add(order);
 		
+		Trader trader = order.getTrader();
+		trader.receiveMessage("Your order for " + order.getSymbol() +
+				"has been placed");
 	}
 	
 }
