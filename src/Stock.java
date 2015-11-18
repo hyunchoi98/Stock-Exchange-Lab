ea3c710a0d838b2b7213e44d20a88825cd30f27a
hyunchoi98 <hyunchoi98@gmail.com>
2015-11-17 21:54:29 -0500

    curly braces

--- src/Stock.java
+++ src/Stock.java
@@ -2,30 +2,24 @@ import java.util.Comparator;
 import java.util.PriorityQueue;
 import java.util.Queue;
 
-
 //Represents a stock in the SafeTrade project
 
 public class Stock {
-	
-	private String stockSymbol; 
+
+	private String stockSymbol;
 	private String companyName;
 	private double lowestSell;
 	private double highestSell;
 	private int volumeDay;
 	private double stockPrice;
-	Queue <TradeOrder> buy;
-	Queue <TradeOrder> sell;
-	
-	
-	
-	
+	Queue<TradeOrder> buy;
+	Queue<TradeOrder> sell;
 
-	//Constructs a new stock with a given symbol, 
-	//company name, and starting price.
-	//creates a priority queue to sell and buy orders both with  
-	//PriceComparator method
-	public Stock(String symbol, String name, double price)
-	{
+	// Constructs a new stock with a given symbol,
+	// company name, and starting price.
+	// creates a priority queue to sell and buy orders both with
+	// PriceComparator method
+	public Stock(String symbol, String name, double price) {
 		stockSymbol = symbol;
 		companyName = name;
 		lowestSell = price;
@@ -36,122 +30,114 @@ public class Stock {
 		sell = new PriorityQueue<TradeOrder>(10, new PriceComparator());
 	}
 
-	//Returns a quote string for this stock. 
-	//includes company name, stock symbol, last sale price
-	//lowest and highest day prices, lowest price in a sell order,
-	//the number of shares in it, highest price in buy order and num shares
-	//in it
-	
+	// Returns a quote string for this stock.
+	// includes company name, stock symbol, last sale price
+	// lowest and highest day prices, lowest price in a sell order,
+	// the number of shares in it, highest price in buy order and num shares
+	// in it
+
 	public String getQuote() {
-		//sample output
-		//Giggle.com (GGGL) Price: 12.00 hi: 14.50 lo: 9.00 vol: 
-		//500 Ask: none Bid: 12.50 size: 200
-		
-		String str = companyName + " (" + stockSymbol + ")" + " Price: " + stockPrice +
-				"\nhi: " + highestSell + " low: " + lowestSell + " vol: " + volumeDay +"\nAsk: ";
-		
-		if (sell.peek()!=null) {
+		// sample output
+		// Giggle.com (GGGL) Price: 12.00 hi: 14.50 lo: 9.00 vol:
+		// 500 Ask: none Bid: 12.50 size: 200
+
+		String str = companyName + " (" + stockSymbol + ")" + " Price: "
+				+ stockPrice + "\nhi: " + highestSell + " low: " + lowestSell
+				+ " vol: " + volumeDay + "\nAsk: ";
+
+		if (sell.peek() != null) {
 			str += sell.peek().getPrice();
 			str += " Size: ";
 			str += sell.peek().getShares();
-		}
-		else {
+		} else {
 			str += "None ";
 		}
-		
-		str+= " Bid: ";
-		if (buy.peek()!=null) {
-			str+= buy.peek().getPrice();
+
+		str += " Bid: ";
+		if (buy.peek() != null) {
+			str += buy.peek().getPrice();
 			str += " Size: ";
 			str += buy.peek().getShares();
-		}
-		else {
+		} else {
 			str += "None";
 		}
-		
+
 		return str;
-		
+
 	}
-	
-	//Places a trading order, adds order to apporpirate priority 
-	//queue depending on if it is a buy or sell. Notifies the trader 
-	//that placed the order that it has been placed by sending a message
+
+	// Places a trading order, adds order to apporpirate priority
+	// queue depending on if it is a buy or sell. Notifies the trader
+	// that placed the order that it has been placed by sending a message
 	public void placeOrder(TradeOrder order) {
 		if (order.isSell())
 			sell.add(order);
 		if (order.isBuy())
 			buy.add(order);
-		
+
 		Trader trader = order.getTrader();
-		trader.receiveMessage("Your order for " + order.getSymbol() +
-				" has been placed");
-		
-	    if (order.isSell())
-	    	if (buy.size() > 0)
-		executeOrder(order);
-	    if (order.isBuy())
-	    	if (sell.size() > 0)
-		executeOrder(order);
-		
-	}
-	
-	private void executeOrder(TradeOrder order)
-	{
-		
-		
-		int shares = order.getShares();
+		trader.receiveMessage("Your order for " + order.getSymbol()
+				+ " has been placed");
+
 		if (order.isSell())
-		{
-			for (TradeOrder price: buy)
-			{
-			if (order.getPrice() <= price.getPrice())
-			{	
-				
-			TradeOrder toSell = buy.remove();
-			
-			toSell.subtractShares(shares);
-			
-			if (toSell.getShares() > 0)
-				buy.add(toSell);
-			
-			Trader seller = order.getTrader();
-			seller.receiveMessage("You sold " + shares +
-					" shares of " + stockSymbol+ " for " + order.getPrice()
-					+ "$ a share");
-			
-			Trader buyer = price.getTrader();
-			buyer.receiveMessage("You bought " + shares +
-					" shares of " + stockSymbol + " for " + order.getPrice()
-					+ "$ a share");
+			if (buy.size() > 0)
+				executeOrder(order);
+		if (order.isBuy())
+			if (sell.size() > 0)
+				executeOrder(order);
+
+	}
+
+	private void executeOrder(TradeOrder order) {
+
+		int shares = order.getShares();
+		System.out.println(shares);
+		if (order.isSell()) {
+			for (TradeOrder price : buy) {
+				if (order.getPrice() <= price.getPrice()) {
+
+					TradeOrder toSell = buy.remove();
+
+					toSell.subtractShares(shares);
+
+					if (toSell.getShares() > 0)
+						buy.add(toSell);
+
+					Trader seller = order.getTrader();
+					seller.receiveMessage("You sold " + shares + " shares of "
+							+ stockSymbol + " for " + order.getPrice()
+							+ "$ a share");
+
+					Trader buyer = price.getTrader();
+					buyer.receiveMessage("You bought " + shares + " shares of "
+							+ stockSymbol + " for " + order.getPrice()
+							+ "$ a share");
+				}
 			}
 		}
-		
-		if (order.isBuy())
-		for (TradeOrder price: sell)
-		{	
-			if (order.getPrice() <= price.getPrice())
+		if (order.isBuy()){
+			for (TradeOrder price : sell) {
+				if (order.getPrice() <= price.getPrice()) {
+					TradeOrder toBuy = sell.remove();
 
-			{
-			TradeOrder toBuy = sell.remove();
-			
-			toBuy.subtractShares(shares);
-			
-			if (toBuy.getShares() > 0)
-				sell.add(toBuy);	
-			
-			Trader buyer = order.getTrader();
-			buyer.receiveMessage("You bought " + shares +
-					" shares of " + stockSymbol);
-			
-			Trader seller = price.getTrader();
-			seller.receiveMessage("You sold " + shares +
-					" shares of " + stockSymbol + " for " + order.getPrice()
-					+ "$ a share");
+					toBuy.subtractShares(shares);
+
+					if (toBuy.getShares() > 0)
+						sell.add(toBuy);
+
+					Trader buyer = order.getTrader();
+					buyer.receiveMessage("You bought " + shares
+							+ " shares of " + stockSymbol);
+
+					Trader seller = price.getTrader();
+					seller.receiveMessage("You sold " + shares
+							+ " shares of " + stockSymbol + " for "
+							+ order.getPrice() + "$ a share");
+				}
+
+			}
 		}
 
 		
 	}
-	
-	}
-	}
-	}
+}
